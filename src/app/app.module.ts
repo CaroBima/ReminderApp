44e6306fc44ab56393ad485317ea63d5025e30ca
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-07-21 15:25:06 -0300

    Agregando el componente about y modificando el modulo de routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     TasksComponent,
     TasksItemComponent,
     AddTaskComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
